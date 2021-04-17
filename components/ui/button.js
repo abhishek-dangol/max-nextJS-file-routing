bd76010d0e0a616d93e7350cc0651da04989a16d
Abhishek Dangol <abhi.dangol@gmail.com>
2021-04-17 00:34:12 -0500

    added icons

---
+++ components/ui/button.js
@@ -0,0 +1,12 @@
+import classes from "./button.module.css";
+import Link from "next/link";
+
+function Button(props) {
+  return (
+    <Link href={props.link}>
+      <a className={classes.btn}>{props.children}</a>
+    </Link>
+  );
+}
+
+export default Button;
