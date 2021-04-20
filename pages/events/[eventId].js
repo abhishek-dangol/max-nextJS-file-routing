10768942e83a218f1b093a9810e4c705f2c75cf6
Abhishek Dangol <abhi.dangol@gmail.com>
2021-04-19 23:09:36 -0500

    added event detail components

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,7 +1,32 @@
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+import EventSummary from "../../components/event-detail/event-summary";
+import { getEventById } from "../../dummy-data";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
+
 function EventDetailPage() {
+  const router = useRouter();
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>No event found!</p>;
+  }
   return (
     <div>
-      <h1>Single event page</h1>
+      <Fragment>
+        <EventSummary title={event.title} />
+        <EventLogistics
+          date={event.date}
+          address={event.location}
+          image={event.image}
+          alt={event.title}
+        />
+        <EventContent>
+          <p>{event.description}</p>
+        </EventContent>
+      </Fragment>
     </div>
   );
 }
